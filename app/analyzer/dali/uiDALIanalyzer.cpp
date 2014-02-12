e9b44b93a94161707393bff1169274b70adcfe73
fordp2002 <SimonEllwood@hotmail.com>
2014-02-12 08:01:05 +0000

    Change Atttibution

--- app/analyzer/dali/uiDALIanalyzer.cpp
+++ app/analyzer/dali/uiDALIanalyzer.cpp
@@ -1,5 +1,6 @@
 /*
- *  Copyright 2013 Embedded Artists AB
+ *  Copyright 2014 Simon R. Ellwood
+ *  This Analyzer by Simon R. Ellwood
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
